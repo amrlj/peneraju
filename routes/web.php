fb1893302b57868f99c6135e79f369b805f8f93d
Amirul <amiruljamari94@gmail.com>
2026-07-19 11:20:37 +0800

    dashboard route

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Lecturer\SchoolClassController;
 use App\Http\Controllers\Lecturer\SubjectController;
 use App\Http\Controllers\Lecturer\QuestionController;
 use App\Http\Controllers\Lecturer\ResultController;
+use App\Http\Controllers\Lecturer\DashboardController as LecturerDashboard;
 use App\Http\Controllers\Lecturer\ExamController as LecturerExamController;
 use App\Http\Controllers\Student\ExamController  as StudentExamController;
+use App\Http\Controllers\Student\DashboardController as StudentDashboard;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Student\AttemptController;
 
 
@@ -20,12 +23,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Lecturer
     Route::prefix('lecturer')->name('lecturer.')->middleware(['role:lecturer', 'verified'])->group(function () {
+        Route::get('/dashboard', LecturerDashboard::class)->name('dashboard');
         Route::resource('classes', SchoolClassController::class)->except('show');
         Route::resource('subjects', SubjectController::class)->except('show');
         Route::resource('questions', QuestionController::class)->except('show');
@@ -39,7 +45,7 @@ Route::middleware('auth')->group(function () {
     });
 
         Route::prefix('student')->name('student.')->middleware(['role:student', 'verified'])->group(function () {
-
+        Route::get('/dashboard', StudentDashboard::class)->name('dashboard');
         Route::get('/exams', [StudentExamController::class, 'index'])->name('exams.index');
         Route::get('/exams/{exam}', [StudentExamController::class, 'show'])->name('exams.show');
         Route::post('/exams/{exam}/start', [AttemptController::class, 'start'])->name('exams.start');
